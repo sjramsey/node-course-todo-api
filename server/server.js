e486d899b9092ed8b0ccd256d6cc1aba4b62eae1
Ramsey, Scott <ramseys@corptst.anz.com>
2017-02-17 11:29:22 +1100

    set environment variables

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var { User } = require('./models/user');
 
 var app = express();
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
